f461612baf75410698e98d7d68b4dc7943d714f4
FauzanRama11 <fauzanzietugas@gmail.com>
2024-03-01 10:21:21 +0700

    navbar

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,11 +1,10 @@
 import Foot from '../components/footer/footer';
 import Nav from '../components/navbar/navbar';
-import NavHome from '../components/navhome/navhome';
 
 export default function SearchPage() {
     return (
         <div>
-        <NavHome />
+        <Nav />
         <img 
         width='100%'
         src='https://static.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2023/07/14/for-2470344003.jpg
